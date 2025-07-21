765dbc785359d124b150dd2f6fc104298ac39162
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-20 20:52:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -87,4 +87,10 @@ with tabs[1]:
     # Add centroids afterward
     fig = add_centroids_to_umap(fig, filt_df)
     st.plotly_chart(fig, use_container_width=True)
+    st.markdown(
+    "<div style='text-align: center; margin-top: -1.2rem; font-size: 0.9rem; color: gray;'>"
+    "Each dashed line shows where papers were most concentrated that year."
+    "</div>",
+    unsafe_allow_html=True
+    )
     st.caption("Each dashed line shows the centroid of papers for that year (Paper Avg. Concentration)")
