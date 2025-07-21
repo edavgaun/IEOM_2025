94f10df7f05291a611df681ee70582029c453f5b
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-20 20:55:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ with tabs[1]:
     fig, filt_df = plot_umap_scatter(df, selected_years=selected_years, selected_conferences=selected_conferences)
     # Add centroids afterward
     fig = add_centroids_to_umap(fig, filt_df)
-    st.caption('Tools to control and use the visual can be found on the upper right corner of the chart")
+    st.caption('Tools to control and use the visual can be found on the upper right corner of the chart')
     st.plotly_chart(fig, use_container_width=True)
     st.markdown(
     "<div style='text-align: center; margin-top: -1.2rem; font-size: 0.9rem; color: gray;'>"
