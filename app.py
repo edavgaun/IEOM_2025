6d6855e5b734b82385e717c014776fbcfecbe043
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-21 17:51:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ st.markdown("""
 # Show the number of papers loaded
 st.title("📊 IEOM 2025 Explorer")
 st.caption("📘 Based on: Edgar Avalos-Gauna; (2025), *15 Years of IEOM Proceedings*")
-st.caption("Avalos-Gauna, E. (2025). *Tracing AI and Supply Chain Emphasis Across the Global IEOM Landscape*. 2nd IEOM World Congress on Industrial Engineering and Operations Management, Windsor, Ontario, Canada, October 14–16, 2025")
+st.caption("Avalos-Gauna, E. (2025). *Tracing AI and Supply Chain Emphasis Across the Global IEOM Landscape, A Meta-Analysis Under Global Uncertainty*. 2nd IEOM World Congress on Industrial Engineering and Operations Management, Windsor, Ontario, Canada, October 14–16, 2025")
 st.write(f"Loaded {len(df)} papers from the dataset.")
 
 # General Instructions
