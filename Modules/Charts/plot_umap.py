37fb0e272eb6ff8c598cbbabbed0ae94a1686c0d
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-20 21:01:37 -0500

    Update plot_umap.py

--- Modules/Charts/plot_umap.py
+++ Modules/Charts/plot_umap.py
@@ -41,6 +41,7 @@ def plot_umap_scatter(
         opacity=0.65
     )
     fig.update_layout(
+    height=800,
     title="UMAP Projection of IEOM Papers Across Conference Regions and Years<br><span style='font-size:14px; font-weight:normal'>(Tools to control and use the visual can be found on the upper right corner of the chart  → )</span>",
     legend_title_text="Year and Conference",
     xaxis_title=None,
