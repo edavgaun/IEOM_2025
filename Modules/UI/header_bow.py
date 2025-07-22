6fcd45e78f29b0b6b57c84c9eb3b8190a6bfcc5c
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-22 13:52:55 -0500

    Update header_bow.py

--- Modules/UI/header_bow.py
+++ Modules/UI/header_bow.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def show_bow_header():
-    st.title("🔤 BOW Frequency Explorer")
+def show_bow_header(text_title):
+    st.title(text_title)
     st.caption("📘 Based on: Edgar Avalos-Gauna (2025), *15 Years of IEOM Proceedings*")
     st.caption("Avalos-Gauna, E. (2025). *Tracing AI and Supply Chain Emphasis Across the Global IEOM Landscape, A Meta-Analysis Under Global Uncertainty*. 2nd IEOM World Congress on Industrial Engineering and Operations Management, Windsor, Ontario, Canada, October 14–16, 2025")
