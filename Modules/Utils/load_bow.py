ca912c325446a989c9d0fb2383ffa73739623be5
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-22 15:42:36 -0500

    Update load_bow.py

--- Modules/Utils/load_bow.py
+++ Modules/Utils/load_bow.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 @st.cache_data
 def load_bow(region: str, year: int) -> pd.DataFrame:
-    file_path = f"Data/parquet/bow_{region.lower()}_{year}.parquet"
+    file_path = f"Data/parquet/ieom_bow_{region.lower()}_{year}.parquet"
     return pd.read_parquet(file_path)
