00d20357b527435fdc30e5e9664e7bcc824bb80c
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-22 15:28:06 -0500

    Create load_bow.py

---
+++ Modules/Utils/load_bow.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+
+@st.cache_data
+def load_bow(region: str, year: int) -> pd.DataFrame:
+    file_path = f"Data/bow_{region.lower()}_{year}.parquet"
+    return pd.read_parquet(file_path)
