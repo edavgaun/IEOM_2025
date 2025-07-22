c3132d1c15b22ec04cce53afe1fc20d22c7126d9
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-22 15:27:45 -0500

    Update load_data.py

--- Modules/Utils/load_data.py
+++ Modules/Utils/load_data.py
@@ -4,7 +4,3 @@ import streamlit as st
 @st.cache_data
 def load_metadata():
     return pd.read_json("Data/ieom_full.json.gz", compression="gzip")
-
-@st.cache_data
-def load_bow():
-    return pd.read_parquet("Data/ieom_bow.parquet")
