3f56d18bc0d9317180d6a7874b4463071a5b1df5
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-23 13:53:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
 import streamlit as st
 from Modules.UI.layout_config import set_layout
 from Modules.UI.header import show_header
+from Modules.UI.instructions import show_main_instructions
+
 
 # Layout and Header
 set_layout()
 show_header("🌍 IEOM 2025 Explorer Suite")
 
 # Welcome Message
-st.markdown("""
-### 🧭 Choose a Visualization Tool
+show_main_instructions()
 
-This dashboard suite helps you explore over 11,000 IEOM conference papers using two complementary apps.
-
-- 🧭 **UMAP Embedding Explorer**  
-  Explore papers in a 2D space based on semantic similarity.  
-  👉 [Open UMAP Explorer](https://ieom-2025-umap.streamlit.app/)
-
-- 🔤 **BOW Frequency Explorer**  
-  View the most frequent words in selected subsets of papers.  
-  👉 [Open BOW Explorer](https://ieom-2025-bow.streamlit.app/)
-""", unsafe_allow_html=True)
-
-st.markdown("---")
 st.markdown("""
 📘 **Citation**  
 Avalos-Gauna, E. (2025). *Tracing AI and Supply Chain Emphasis Across the Global IEOM Landscape, A Meta-Analysis Under Global Uncertainty*.  
