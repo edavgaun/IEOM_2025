b842e1556786abfa021fee77c1f8e7ae7cb3addf
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-30 18:20:09 -0500

    Update drift_chart.py

--- Modules/Charts/drift_chart.py
+++ Modules/Charts/drift_chart.py
@@ -107,7 +107,7 @@ def semmantic_drift_plot_plotly(region, year, tf_dfs, tf_idfs,
                        font=dict(size=fz, color='#ff7f0e'), showarrow=False)
     fig.add_annotation(xref="paper", yref="paper", x=0.95, y=0.99, text="Frequent + Distinctive",
                        font=dict(size=fz, color='#2ca02c'), showarrow=False)
-    fig.add_annotation(xref="paper", yref="paper", x=0.45, y=0.075, text="Noise",
+    fig.add_annotation(xref="paper", yref="paper", x=0.425, y=0.075, text="Noise",
                        font=dict(size=fz, color='#7f7f7f'), showarrow=False)
     fig.add_annotation(xref="paper", yref="paper", x=0.95, y=0.075, text="Generic but Common",
                        font=dict(size=fz, color='#1f77b4'), showarrow=False)
