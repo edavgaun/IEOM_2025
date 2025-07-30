3457cf047eeee07cdd0346530585cd5d5d0450e1
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-30 17:30:35 -0500

    Update drift_chart.py

--- Modules/Charts/drift_chart.py
+++ Modules/Charts/drift_chart.py
@@ -47,7 +47,8 @@ def semmantic_drift_plot_plotly(region, year, tf_dfs, tf_idfs,
         mode='markers + text',
         name='Keywords',
         marker=dict(color='red', size=9),
-        text=[w.upper().replace(' ', '\n') for w in keyword_index],
+        textfont=dict(color='red', size=fz-4)
+        text=[w.upper().replace(' ', '<br>') for w in keyword_index],
         hovertemplate="<b>%{text}</b><br>TF=%{x}<br>TF-IDF=%{y}<extra></extra>"
     ))
 
