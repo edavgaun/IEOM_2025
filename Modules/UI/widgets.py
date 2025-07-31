67f4df0bd5afe46f2c1e89748da0e2d794000fe2
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-30 20:50:14 -0500

    Update widgets.py

--- Modules/UI/widgets.py
+++ Modules/UI/widgets.py
@@ -32,9 +32,9 @@ def show_tfidf_widgets(conferences, tf_dfs):
         keywords_input = st.multiselect(
             "Select KeyWords", options=available_words, 
             default=[
-                    # AI & Machine Learning
+                    # AI & Machine Learning #"neural networks",
                     "generative ai", "ai", "machine learning", "llm", "reinforcement learning",
-                    "deep learning", #"neural networks", "computer vision", "natural language processing",
+                    "deep learning", "computer vision", "natural language processing",
                     # Supply Chain & Logistics
                     #"supply chain", "smart logistics", "logistics", "lean manufacturing", "green supply chain",
                     #"six sigma", "tqm", "agile", "warehouse layout", "inventory management", "circular economy",       
