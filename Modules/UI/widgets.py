1742e7606ab2a3966b252b488d225f29c6f4956a
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-07-30 20:46:39 -0500

    Update widgets.py

--- Modules/UI/widgets.py
+++ Modules/UI/widgets.py
@@ -31,29 +31,29 @@ def show_tfidf_widgets(conferences, tf_dfs):
         st.markdown("### 🔍 Keywords to Highlight")
         keywords_input = st.multiselect(
             "Select KeyWords", options=available_words, 
-            default=selected_keywords = [
-                                        # AI & Machine Learning
-                                        "generative ai", "ai", "machine learning", "llm", "reinforcement learning",
-                                        "deep learning", "neural networks", "computer vision", "natural language processing",
-                                        # Supply Chain & Logistics
-                                        "supply chain", "smart logistics", "logistics", "lean manufacturing", "green supply chain",
-                                        "six sigma", "tqm", "agile", "warehouse layout", "inventory management", "circular economy",       
-                                        # Digital Transformation & Technologies
-                                        "digital transformation", "digital twin", "blockchain", "iot", "internet thing",
-                                        "cloud computing", "automation", "digital", "digital thread",
-                                        # Data & Analytics
-                                        "data", "big data", "clustering", "data analytics", "business intelligence",
-                                        "data-driven", "data governance", "analytics maturity",
-                                        # Optimization & Modeling
-                                        "optimization", "simulation", "linear programming", "queuing theory",
-                                        # Human & Management
-                                        "management", "leadership", "ethic", "ethical", "kpis", "education",
-                                        # Sustainability & Resilience
-                                        "sustainability", "carbon footprint", "energy efficiency", "sustainable development",
-                                        "green manufacturing", "resilience", "uncertainty",
-                                        # Domain-Specific
-                                        "covid", "healthcare", 'ukraine', 'suez', 'trade war', 'panama'
-                                    ]
+            default=[
+                    # AI & Machine Learning
+                    "generative ai", "ai", "machine learning", "llm", "reinforcement learning",
+                    "deep learning", "neural networks", "computer vision", "natural language processing",
+                    # Supply Chain & Logistics
+                    "supply chain", "smart logistics", "logistics", "lean manufacturing", "green supply chain",
+                    "six sigma", "tqm", "agile", "warehouse layout", "inventory management", "circular economy",       
+                    # Digital Transformation & Technologies
+                    "digital transformation", "digital twin", "blockchain", "iot", "internet thing",
+                    "cloud computing", "automation", "digital", "digital thread",
+                    # Data & Analytics
+                    "data", "big data", "clustering", "data analytics", "business intelligence",
+                    "data-driven", "data governance", "analytics maturity",
+                    # Optimization & Modeling
+                    "optimization", "simulation", "linear programming", "queuing theory",
+                    # Human & Management
+                    "management", "leadership", "ethic", "ethical", "kpis", "education",
+                    # Sustainability & Resilience
+                    "sustainability", "carbon footprint", "energy efficiency", "sustainable development",
+                    "green manufacturing", "resilience", "uncertainty",
+                    # Domain-Specific
+                    "covid", "healthcare", 'ukraine', 'suez', 'trade war', 'panama'
+                ]
 
         )
 
